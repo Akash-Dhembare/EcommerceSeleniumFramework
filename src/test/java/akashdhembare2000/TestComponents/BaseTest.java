434f0796da179992844e3257296d8a3f0d7e9802
Akash-Dhembare <akash.dhembare777@gmail.com>
2025-06-08 19:09:17 +0530

    .

--- src/test/java/akashdhembare2000/TestComponents/BaseTest.java
+++ src/test/java/akashdhembare2000/TestComponents/BaseTest.java
@@ -4,12 +4,16 @@ import akashdhembare2000.pageObjects.LandingPage;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -38,18 +42,36 @@ public class BaseTest {
         String browserName=  System.getProperty("browser")!=null ? System.getProperty("browser") : prop.getProperty("browser");
 
 
-        if(browserName.equalsIgnoreCase("chrome")){
-            driver= new ChromeDriver();
+        if(browserName.contains("chrome")){
+            ChromeOptions options= new ChromeOptions();
+            if(browserName.contains("headless")) {
+                options.addArguments("headless");
+            }
+
+            driver= new ChromeDriver(options);
+            driver.manage().window().setSize(new Dimension(1440, 900)); // Help to run test cases in fullscreen in headless mode
 
         } else if (browserName.equalsIgnoreCase("edge")) {
-               driver= new EdgeDriver();
+            EdgeOptions edgeOptions= new EdgeOptions();
+            if(browserName.contains("headless")) {
+                edgeOptions.addArguments("headless");
+            }
+
+            driver= new EdgeDriver(edgeOptions);
+            driver.manage().window().setSize(new Dimension(1440, 900));
 
         } else if (browserName.equalsIgnoreCase("firefox")){
-           driver= new FirefoxDriver();
+            FirefoxOptions options= new FirefoxOptions();
+            if(browserName.contains("headless")) {
+                options.addArguments("headless");
+            }
+           driver= new FirefoxDriver(options);
+           driver.manage().window().setSize(new Dimension(1440, 900));
 
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
 
         return driver;
     }
